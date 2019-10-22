54299da28a1ebf804f73752a550ebe2f2295dcbf
Antoine Williams <54339074+antoineel9@users.noreply.github.com>
2019-10-22 13:30:23 -0500

    Add files via upload

---
+++ Data/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeadCount.Data
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int EmpId { get; set; }
+        public int DeptId { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Date{ get; set; }
+        public string Approved { get; set; }
+        public int URId { get; set; }
+        public bool? ApprPos { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+
+    }
+}
