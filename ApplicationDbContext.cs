106b946eddb952de6acc2c09e6d91e43699cb97b
Antoine Williams <54339074+antoineel9@users.noreply.github.com>
2019-10-22 13:27:16 -0500

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using HeadCount.Data;
+
+namespace HeadCount.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<HeadCount.Data.Department> Department { get; set; }
+        public DbSet<HeadCount.Data.Employee> Employee { get; set; }
+        public DbSet<HeadCount.Data.Audit> Audit { get; set; }
+    }
+}
---
+++ Department.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeadCount.Data
+{
+    public class Department
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int DeptId { get; set; }
+        public string Name { get; set; }
+        public string DeptHead{ get; set; }
+        public string DeptLoc{ get; set; }
+
+        //foreign key connect Employees table
+
+        public int EmpId { get; set; }
+        [ForeignKey("EmpId")]
+        public Employee Employees { get; set; }
+
+    }
+
+}
